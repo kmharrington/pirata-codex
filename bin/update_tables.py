d2afce9c406689ac13eee081757b96e495ca95a2
Katie Harrington <katie.megan.harrington@gmail.com>
2018-12-01 21:49:51 -0500

    setup update_tables to send messages

--- bin/update_tables.py
+++ bin/update_tables.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+
 import pirata_codex as pc
 import pirata_codex.database_utils as utils
 
-# Verify that I haven't added new clans to track.
-# Updates the Clan table
-utils.verify_clan_table()
-# Update the Clan_Data and Player tables
-utils.update_clan_data()
-# Look for missing people
-utils.flag_missing_players()
-# Update the Player_Data table
-utils.update_player_data()
+discord = pc.Discord()
+try:
+    # Verify that I haven't added new clans to track.
+    # Updates the Clan table
+    utils.verify_clan_table()
+    # Update the Clan_Data and Player tables
+    message = utils.update_clan_data()
+    # Look for missing people
+    message += utils.flag_missing_players()
+    # Update the Player_Data table
+    message += utils.update_player_data()
+except:
+    discord.send('Clash API pulls and/or Database update failed\n'+message)
+else:
+    discord.send('Database Update was successful\n'+message)
